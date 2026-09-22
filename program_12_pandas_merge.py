858ebc47e0bbf5e1dc41a49e2f0e104c6dbdbb07
anjalig2006-web <anjalig.2006@gmail.com>
2026-09-22 21:13:49 +0530

    Create program_12_pandas_merge.py

---
+++ program_12_pandas_merge.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+expression = pd.DataFrame({
+    "Sample": ["S1", "S2", "S3", "S4"],
+    "Gene_A": [10, 12, 20, 22],
+    "Gene_B": [15, 18, 25, 30]
+})
+
+metadeta = pd.DataFrame({
+    "Sample": ["S1", "S2", "S3", "S4"],
+    "Age": [21, 25, 22, 24],
+    "Condition": ["Control", "Control", "Treated", "Treated"]
+})
+
+merged_data = pd.merge(expression, metadeta, on="Sample")
+
+print (merged_data)
