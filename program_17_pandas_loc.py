970244a92fa4bceac9151ed93083028af7d4b5a5
anjalig2006-web <anjalig.2006@gmail.com>
2026-09-26 13:36:40 +0530

    Create program_17_pandas_loc.py

---
+++ program_17_pandas_loc.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Sample": ["S1", "S2", "S3", "S4", "S5", "S6"],
+    "Gene_A": [10, 15, 12, 25, 20, 18],
+    "Gene_B": [14, 18, 16, 30, 22, 21],
+    "Condition": ["Control", "Control", "Control", "Treated", "Treated", "Treated"]
+}
+
+df = pd.DataFrame(data)
+
+s4_data = df.loc [df["Sample"] == "S4"]
+
+print(s4_data)
